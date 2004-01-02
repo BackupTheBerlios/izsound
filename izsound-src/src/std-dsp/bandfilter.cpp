1f77388f9461bc7746ea75ab71e06b12babb1e5d
jponge <jponge>
2004-01-02 17:21:59 +0000

    New year copyright updates.

--- izsound-src/src/std-dsp/bandfilter.cpp
+++ izsound-src/src/std-dsp/bandfilter.cpp
@@ -1,5 +1,5 @@
 /*
- * IzSound - Copyright (c) 2003 Julien PONGE - All rights reserved.
+ * IzSound - Copyright (c) 2003, 2004 Julien PONGE - All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- izsound-src/stripdown.py
+++ izsound-src/stripdown.py
@@ -6,7 +6,7 @@
 # equivalent) repository should only contain the files left after running a
 # 'make distclean' and this script.
 #
-# IzSound - Copyright (c) 2003 Julien PONGE - All rights reserved.
+# IzSound - Copyright (c) 2003, 2004 Julien PONGE - All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
